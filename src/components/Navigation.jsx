c3df15af0bfdbd61bb05229bd855771a848bcecf
GarbaGeCode1984 <rkqlwl1984@gmail.com>
2022-12-22 14:55:10 +0900

    navbar 반응형 추가

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,7 +6,50 @@ import { Link } from "react-scroll";
 function Navigation() {
   return (
     <>
-      <Navbar bg="light" variant="light" sticky="top" className="navbar">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        sticky="top"
+        className="navbar"
+      >
+        <Container fluid>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav" fluid>
+            <Nav className="me-auto  navWrap" lg="8">
+              <Link to="home">
+                <Nav.Link className="navItem" href="#home">
+                  HOME
+                </Nav.Link>
+              </Link>
+              <Link to="about">
+                <Nav.Link className="navItem" href="#about">
+                  ABOUT
+                </Nav.Link>
+              </Link>
+              <Link to="skilles">
+                <Nav.Link className="navItem" href="#skills">
+                  SKILLS
+                </Nav.Link>
+              </Link>
+              <Link to="project">
+                <Nav.Link className="navItem" href="#project">
+                  PROJECT
+                </Nav.Link>
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      ;
+    </>
+  );
+}
+
+export default Navigation;
+
+/*  <Navbar bg="light" variant="light" sticky="top" className="navbar">
         <Container fluid>
           <Nav className="me-auto navWrap" lg="8">
             <Link to="home">
@@ -31,9 +74,4 @@ function Navigation() {
             </Link>
           </Nav>
         </Container>
-      </Navbar>
-    </>
-  );
-}
-
-export default Navigation;
+      </Navbar> */
