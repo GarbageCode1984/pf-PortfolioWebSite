840bcdf18b9da32aa73d20bef9c9806766182ac5
GarbageCode1984 <rkqlwl1984@gmail.com>
2022-12-24 13:46:29 +0900

    폰트 움직이게

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import "./scss/home.scss";
 
 function Main() {
-  return (
-    <>
-      <div className="mainImage" id="home">
-        <h1>
-          <p>
-            안녕하세요.
-            <br />
-            프론트엔드 개발자
-            <br />
-            서우석입니다.
-          </p>
-        </h1>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="mainImage" id="home">
+                <h1>
+                    <span>"미완성"</span>
+                    <br />
+                    <span>안녕하세요.</span>
+                    <br />
+                    <span>프론트엔드 개발자</span>
+                    <br />
+                    <span>서우석입니다.</span>
+                </h1>
+            </div>
+        </>
+    );
 }
 
 export default Main;
