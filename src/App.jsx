4170c40381de7f65cdc5de25a12cb44e5a326c1b
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-03-22 02:33:03 +0900

    포트폴리오 사이트 Json 파일로 따로 빼내고 관리, 포폴 상세부분 디자인 및 내용 수정

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ import Project from "./components/Project";
 import Skilles from "./components/Skilles";
 import "./components/scss/reset.scss";
 function App() {
-  return (
-    <>
-      <Home />
-      <Navigation />
-      <About />
-      <Skilles />
-      <Project />
-    </>
-  );
+    return (
+        <>
+            <Home />
+            <Navigation />
+            <About />
+            <Skilles />
+            <Project />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/ProjectCard2.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+} from "reactstrap";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./scss/projectCard.scss";
+
+function ProjectCard({ items }) {
+  return (
+    <div>
+      <div>
+        <Card className="cardWrap">
+          <CardImg src={items.mainImage} alt="Card image" />
+          <CardBody className="d-flex flex-column align-items-center">
+            <CardTitle className="cardName">{items.name}</CardTitle>
+            <CardSubtitle className="cardHash">{items.hash}</CardSubtitle>
+            <CardText className="cardText">{items.skilles}</CardText>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cardRes"
+              href={items.res}
+            >
+              <Button size="sm" variant="secondary">
+                소스코드
+              </Button>
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="webLink"
+              href={items.webLink}
+            >
+              <Button size="lg" variant="outline-dark">
+                WEB
+              </Button>
+            </a>
+          </CardBody>
+        </Card>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
