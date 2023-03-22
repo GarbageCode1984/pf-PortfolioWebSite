394106d469f8f4423299082d1741bd97e9d4a96c
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-03-22 20:12:58 +0900

    포트폴리오 부분 디자인 수정

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -17,17 +17,22 @@ function ProjectCard({ items }) {
                             <p>Role : </p>
                             <div>{items.role}</div>
                         </CardSubtitle>
-                        <CardText className="cardStack">{items.stack}</CardText>
-                        <a target="_blank" rel="noopener noreferrer" className="cardRes" href={items.res}>
-                            <Button size="sm" variant="secondary">
-                                소스코드
-                            </Button>
-                        </a>
-                        <a target="_blank" rel="noopener noreferrer" className="webLink" href={items.webLink}>
-                            <Button size="lg" variant="outline-dark">
-                                WEB
-                            </Button>
-                        </a>
+                        <CardText className="cardStack">
+                            <p>Stack : </p>
+                            <div>{items.stack}</div>
+                        </CardText>
+                        <div className="resWebWrap">
+                            <a target="_blank" rel="noopener noreferrer" className="cardRes" href={items.res}>
+                                <Button size="lg" variant="outline-danger">
+                                    Code
+                                </Button>
+                            </a>
+                            <a target="_blank" rel="noopener noreferrer" className="webLink" href={items.webLink}>
+                                <Button size="lg" variant="outline-dark">
+                                    WEB
+                                </Button>
+                            </a>
+                        </div>
                     </CardBody>
                 </Card>
             </div>
